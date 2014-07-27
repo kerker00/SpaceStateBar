2f78c52e86c7bbc8d71d171c10798a4ef389360f
Markus Müller <markus@grafixmafia.net>
2014-07-27 20:34:23 +0200

    start creating preference window

---
+++ HackSpaceStatusBar/PreferencePane.swift
@@ -0,0 +1,13 @@
+//
+//  PreferencePane.swift
+//  MainFrameStatusBar
+//
+//  Created by markus on 27.07.14.
+//  Copyright (c) 2014 grafixmafia.net. All rights reserved.
+//
+
+import Cocoa
+
+class preferencePane {
+
+}
